9682fec36176a9501ed547737ce822f7dc10639b
Tom Schumm <fwiffo@gmail.com>
2024-10-06 19:16:46 -0700

    Add config for D3AA with SFT-25R.

---
+++ hw/hank/emisar-d3aa/config-sft-25r.h
@@ -0,0 +1,19 @@
+/*
+ * D3AA with single channel Luminus SFT-25R.
+ * Dark gray with copper accents and cool white switch.
+ * Channel 1: Luminus SFT-25R 6500K.
+ *
+ * Turbo:  7601K, Duv -0.0010
+ * High:   7264K, Duv  0.0006
+ * Medium: 7318K, Duv  0.0014
+ * Low:    6532K, Duv  0.0014
+ */
+
+#include "hank/config-fwiffo.h"
+
+#undef DEFAULT_JUMP_START_LEVEL
+#define DEFAULT_JUMP_START_LEVEL 10  // TODO: tune better.
+
+// Enable RGB AUX channels.
+#undef CHANNEL_MODES_ENABLED
+#define CHANNEL_MODES_ENABLED 0b0000000011111111
