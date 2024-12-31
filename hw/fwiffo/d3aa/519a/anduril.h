6ee4f275982407a30c3df28af2ec01b0fe857dec
Tom Schumm <fwiffo@gmail.com>
2024-12-31 14:47:51 -0800

    Adjust moonlight levels for newer boost drivers since level 1 is very low.

--- hw/fwiffo/d3aa/519a/anduril.h
+++ hw/fwiffo/d3aa/519a/anduril.h
@@ -16,9 +16,12 @@
 #include "hank/emisar-d3aa/anduril.h"
 #include "fwiffo/anduril.h"
 
-#undef DEFAULT_JUMP_START_LEVEL
-#define DEFAULT_JUMP_START_LEVEL 10  // TODO: tune better.
-
 // Enable RGB AUX channels.
 #undef CHANNEL_MODES_ENABLED
 #define CHANNEL_MODES_ENABLED 0b0000000011111111
+
+// The lowest level for Thefreeman's boost driver is too low for moonlight.
+#undef RAMP_DISCRETE_FLOOR
+#undef RAMP_SMOOTH_FLOOR
+#define RAMP_DISCRETE_FLOOR 5
+#define RAMP_SMOOTH_FLOOR 5
--- hw/fwiffo/da1k/xhp703hi/anduril.h
+++ hw/fwiffo/da1k/xhp703hi/anduril.h
@@ -14,3 +14,9 @@
 // Enable RGB AUX channels.
 #undef CHANNEL_MODES_ENABLED
 #define CHANNEL_MODES_ENABLED 0b0000000011111111
+
+// The lowest level for the Lume X1 driver is too low for moonlight.
+#undef RAMP_DISCRETE_FLOOR
+#undef RAMP_SMOOTH_FLOOR
+#define RAMP_DISCRETE_FLOOR 3
+#define RAMP_SMOOTH_FLOOR 3
